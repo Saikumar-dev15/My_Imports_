4894876165f07125442af8fdbfd4d62c1ee6fdf7
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-07-17 23:44:21 +0530

    Verified Decision Classification and Completed Random forest

--- ML Class/Decision Tree.py
+++ ML Class/Decision Tree.py
@@ -19,12 +19,18 @@ max_depth = 5,10,15
 
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_breast_cancer
+import pandas as pd
 
-x="max_depth"
-y= "criterion"
+df = pd.read_csv("ML Class/students.csv")
 
-x_train, x_test, y_train , y_test_reg = train_test_split(
-    x,y, train_size=0.8, random_state=42
+df["Pass"] = (df["Final_Marks"] >= 50).astype(int)
+
+x = df[["Previous_Marks"]]
+y = df["Pass"]
+
+x_train, x_test, y_train , y_test = train_test_split(
+    x,y, test_size=0.2, random_state=42
 )
 
 model = DecisionTreeClassifier()
@@ -34,8 +40,64 @@ paras = {
     "criterion": ["gini", "entropy"]
 }
 
-grid = GridSearchCV(model, paras, cv=3)
+grid = GridSearchCV(
+    estimator=model,
+    param_grid=paras,
+    cv=3,)                            #our data is divided  into 3 parts. (2- training and 1- testing)
 grid.fit(x_train, y_train)
 
 print(grid.best_params_)
 print(grid.best_score_)
+
+
+
+
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
+
+data = load_breast_cancer()
+
+X = pd.DataFrame(data.data, columns=data.feature_names)
+y = data.target
+
+print(X.shape)   # (569, 30)
+print(len(y))    # 569
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+model = RandomForestClassifier(random_state=42)
+
+# Parameters
+param_grid = {
+    "n_estimators": [50, 100, 200],
+    "max_depth": [5, 10, 15],
+    "criterion": ["gini", "entropy"]
+}
+
+# Grid Search
+grid = GridSearchCV(
+    estimator=model,
+    param_grid=param_grid,
+    cv=5,
+    scoring="accuracy"
+)
+
+grid.fit(X_train, y_train)
+
+print("Best Parameters:", grid.best_params_)
+print("Best Score:", grid.best_score_)
+
+y_pred = grid.predict(X_test)
+#print(f"y_pred: ",y_pred)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
